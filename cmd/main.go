ddcfaf7a91191e358d56592f7187331d68786efc
Andre Mello <andre.lmm@gmail.com>
2023-10-30 20:39:25 -0300

    feat: GRPC

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/andrelmm/imersao-fs-fc/codepix-go/application/grpc"
+	"github.com/andrelmm/imersao-fs-fc/codepix-go/infrastructure/db"
+	"github.com/jinzhu/gorm"
+	"os"
+)
+
+var database *gorm.DB
+
+func main() {
+	database = db.ConnectDB(os.Getenv("env"))
+	grpc.StartGrpcServer(database, 50051)
+}
